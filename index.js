dbfdfa19e6d72c4ec036ac562601ffc2fc466b8d
Andrew Davis <andrew@gigantic.computer>
2016-12-20 11:26:51 -0600

    remove twilio

--- index.js
+++ index.js
@@ -2,54 +2,6 @@
 
 var server = require('./routes')
 
-/*
-var callWithTwilio = require('./lib/call-with-twilio').call({
-  twilioSid: settings.twilioSid,
-  twilioToken: settings.twilioToken
-})
-
-server.route({
-  method: 'POST',
-  path: '/call',
-  handler: function (req, reply) {
-    callWithTwilio(req.payload.number)
-    .then(function (reps) {
-      console.log('CALLED', reps)
-      reply(reps)
-    })
-    .catch(function (error) {
-      console.log('error:', error.message)
-      reply(error)
-      throw new Error(error)
-    })
-  }
-})
-
-var retreiveScript = require('./lib/call-with-twilio').retreiveScript({
-  twilioSid: settings.twilioSid,
-  twilioToken: settings.twilioToken
-})
-
-server.route({
-  method: ['GET', 'POST'],
-  path: '/scripts/{uuid}',
-  handler: function (req, reply) {
-    retreiveScript(req.params.uuid)
-    .then(function (script) {
-      console.log('SCRIPT', script)
-      reply(script)
-      .header('Content-Type', 'application/xml')
-    })
-    .catch(function (error) {
-      console.log('error:', error.message)
-      reply(error)
-      throw new Error(error)
-    })
-  }
-})
-
-*/
-
 server.start()
 
 module.exports = server
--- settings.js
+++ settings.js
@@ -2,8 +2,6 @@ require('dotenv').config()
 
 module.exports = {
   sunlightKey: process.env.SUNLIGHT_KEY,
-  twilioSid: process.env.TWILIO_SID,
-  twilioToken: process.env.TWILIO_TOKEN,
   bill: {
     state: process.env.BILL_STATE,
     year: process.env.BILL_YEAR,
