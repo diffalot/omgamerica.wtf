109fbd1c6f202dc17ff61ee567fe60faa6c684ac
Andrew Davis <andrew@gigantic.computer>
2016-12-27 14:00:22 -0600

    build a simple inferno component

---
+++ src/containers/App.js
@@ -0,0 +1,11 @@
+import Inferno from 'inferno'
+import Component from 'inferno-component'
+
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (<p>hello</p>)
+  }
+}
